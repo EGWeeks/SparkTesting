d72e14329775b34fcdd1fa90f8430af4cc79c170
Ethan Weeks <ethanweeks@ymail.com>
2016-04-27 22:56:21 -0600

    initial commit - basic get/post route set up

---
+++ src/main/java/com/course/idea/Main.java
@@ -0,0 +1,42 @@
+package com.course.idea;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+
+            model.put("username", req.cookie("username"));
+
+            return new ModelAndView(model, "index.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
+
+        post("/sign-in", (req, res) -> {
+
+            Map<String, String> model = new HashMap<>();
+
+            String username = req.queryParams("username");
+
+            res.cookie("username", username);
+
+            model.put("username", username);
+
+            return new ModelAndView(model, "sign-in.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
+
+    }
+}
